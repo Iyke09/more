2a15a4aa976f2046659045c6806e8afb05769930
Emma Paris <iykay33@gmail.com>
2017-09-06 23:53:23 +0100

    add users signin route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -49,6 +49,44 @@ router.post('/signup', (req, res) => { // --------------------------checked
     .catch(error => res.status(500).send(error));
 });
 
+router.post('/signin', (req, res) => { // ----------------------------checked
+  if (!req.body.email) {
+    return res.status(400).json({
+      message: 'email field is required',
+    });
+  }
+  if (!req.body.password) {
+    return res.status(400).json({
+      message: 'password field is required',
+    });
+  }
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(400).send({
+          message: 'invalid login details',
+        });
+      }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        return res.status(400).send({
+          message: 'Incorrect password',
+        });
+      }
+      const token = jwt.sign({ user }, 'secret', { expiresIn: 7200 });
+      res.status(200).send({
+        message: 'Successfully logged in',
+        token,
+        userId: user.id,
+      });
+    })
+    .catch(error => res.status(400).send(error));
+},
+);
+
 
 
 module.exports = router;
